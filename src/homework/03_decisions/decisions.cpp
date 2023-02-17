a7dac664a0fef5b1d1e575cebd26746f94fcb73e
LarryG <101243518+GitLarryG@users.noreply.github.com>
2023-02-17 06:39:59 +0000

    Homework 3

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,70 @@
 //write include statement for decisions header
-
+#include "decisions.h"
 
 //Write code for function(s) code here
+string get_letter_grade_using_if(int grade)
+{
+    string letter_grade;
+
+    if (grade >= 90 && grade <= 100)
+    {
+        letter_grade = "A";
+    }
+    else if (grade >= 80 && grade < 90)
+    {
+        letter_grade = "B";
+    }
+    else if (grade >= 70 && grade < 80)
+    {
+        letter_grade = "C";
+    }
+    else if (grade >= 60 && grade < 70)
+    {
+        letter_grade = "D";
+    }
+    else if (grade >= 00 && grade < 60)
+    {
+        letter_grade = "F";
+    }
+    else
+    {
+        letter_grade = "Invalid Grade";
+    }
+
+    return letter_grade;
+}
+
+string get_letter_grade_using_switch(int grade)
+{
+    string letter_grade;
+    int case_grade = grade/10;
+    switch(case_grade)
+    {
+    case 10:
+    case 9:
+        letter_grade = "A";
+        break;
+    case 8:
+        letter_grade = "B";
+        break;
+    case 7:
+        letter_grade = "C";
+        break;
+    case 6:
+        letter_grade = "D";
+        break;
+    case 5:
+    case 4:
+    case 3:
+    case 2:
+    case 1:
+    case 0:
+        letter_grade = "F";
+        break;
+    default:
+        letter_grade = "Invalid Grade";
+        break;
+    }
+
+    return letter_grade;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,55 @@
 //write include statements
+#include "decisions.h"
 
 int main() 
 {
+	int grade = 0;
+	int menu_option = 0;
+	string result;
+	cout<<"MAIN MENU\n";
+	cout<<"1-Letter grade using if\n";
+	cout<<"2-Letter grade using switch\n";
+	cout<<"3-Exit\n\n";
+
+	cin>>menu_option;
+
+	
+	
+
+	switch (menu_option)
+	{
+	case 1:
+		cout<<"Enter grade: ";
+		cin>>grade;
+		if (grade >= 0 && grade <= 100)
+			{
+				cout<<"Letter Grade: "<<get_letter_grade_using_if(grade)<<"\n";
+			}
+		else
+		{
+			cout<<"You entered a number outside of 0-100, please try again.";
+		}
+		break;
+	
+	case 2:
+		cout<<"Enter grade: ";
+		cin>>grade;
+		if (grade >= 0 && grade <= 100)
+		{
+			cout<<"Letter Grade: "<<get_letter_grade_using_switch(grade)<<"\n";
+		}
+		else
+		{
+			cout<<"You entered a number outside of 0-100, please try again.";
+		}
+		break;
+	
+	case 3:
+		cout<<"Exiting...";
+		break;
+	default:
+		break;
+	}
+	
 	return 0;
 }
