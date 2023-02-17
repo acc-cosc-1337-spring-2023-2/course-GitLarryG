431462b1c180bdae028d982a332d5f0f4a94c49f
LarryG <101243518+GitLarryG@users.noreply.github.com>
2023-02-17 06:48:50 +0000

    Homework 3 update1

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -37,31 +37,33 @@ string get_letter_grade_using_if(int grade)
 string get_letter_grade_using_switch(int grade)
 {
     string letter_grade;
+    //to simplify a range of grades into a constant I will divide by 10
+    //because the switch doesn't accept ranges as cases, only constants
     int case_grade = grade/10;
     switch(case_grade)
     {
     case 10:
-    case 9:
+    case 9://90-100 will turn into 9 & 10, recieving A's
         letter_grade = "A";
         break;
-    case 8:
+    case 8://80-89 will turn into 8, recieving B's
         letter_grade = "B";
         break;
-    case 7:
+    case 7://70-79 will turn into 7, recieving C's
         letter_grade = "C";
         break;
-    case 6:
+    case 6://60-69 will turn into 6, recieving D's
         letter_grade = "D";
         break;
-    case 5:
-    case 4:
-    case 3:
-    case 2:
-    case 1:
-    case 0:
+    case 5://50-59 will turn into 5,
+    case 4://40-49 will turn into 4,
+    case 3://30-39 will turn into 3,
+    case 2://20-29 will turn into 2,
+    case 1://10-19 will turn into 1,
+    case 0://0-9 will turn into 0, all will recieve F's
         letter_grade = "F";
         break;
-    default:
+    default://default incase user enters something outside of the range
         letter_grade = "Invalid Grade";
         break;
     }
