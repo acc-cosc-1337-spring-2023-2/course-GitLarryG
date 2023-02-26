1f1f852cf4197abd2537845ba5ed2c44db18e08a
LarryG <101243518+GitLarryG@users.noreply.github.com>
2023-02-26 06:40:46 +0000

    Homework3

---
+++ src/examples/06_strings/02_functions/function_str.cpp
@@ -0,0 +1,9 @@
+#include "function_str.h"
+
+void str_val_param(std::string str1, std::string &str2, const std::string & str3)
+{
+    str1 = "changed";
+    str2 = "changed";
+
+    
+}
--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,33 @@
 //add include statements
-
+#include<iostream>
+#include"dna.h"
+using std::swap;
 //add function(s) code here
+
+int factorial(int num)
+{
+    auto result = 1;
+    int prenum = 1; 
+    while (prenum <= num)
+    {
+        result*=prenum;
+        prenum++;
+    }
+    return result;
+}
+
+int gcd(int num1, int num2)
+{
+    while (num1 != num2)
+    {
+        if(num1 < num2)
+        {
+            swap(num2, num1);
+        }
+        else if(num1 > num2)
+        {
+            num1 = num1 - num2;
+        }
+    }
+    return num1;
+}
